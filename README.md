5cf198267c8e172a5c6f8ab02f12ff53d341d61c
Buğra Aydın <bugraaydin1@hotmail.com>
2022-07-02 15:46:15 +0300

    Init react typescript boilerplate

---
+++ README.md
@@ -0,0 +1,24 @@
+# Rise Tecnology Assesment
+
+## Techstack
+
+###### Frontend
+
+- React
+- Typescript
+- Redux Toolkit
+- Webpack + Babel
+- MUI library + Emotion
+- Tslint
+- Jest
+
+###### Backend
+
+- Node.js
+
+### Start Project
+
+```
+$ npm install
+$ npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./index.tsx",
+  devtool: "inline-source-map",
+  output: {
+    path: path.join(__dirname, "build/dist"),
+    filename: "bundle.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "public",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif|webp)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+      favicon: "public/favicon.ico",
+    }),
+  ],
+};
